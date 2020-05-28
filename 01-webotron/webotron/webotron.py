06a15e5c001e82538a4049feb26d2bb1fae3b1fb
Shawn Wong <luckywwx@gmail.com>
2020-05-28 10:26:41 +0800

    Allow user to set AWS profile

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,111 +1,60 @@
 import boto3
 import click
-from botocore.exceptions import ClientError
-from pathlib import Path
-import mimetypes
 
-session = boto3.Session(profile_name='pythonProject')
-s3 = session.resource('s3')
+from bucket import BucketManager
+
+# session = boto3.Session(profile_name='pythonProject')
+# bucket_manager = BucketManager(session)
+# # s3 = session.resource('s3')
+#
+# @click.group()
+# def cli():
+#     "Webotron deploys websites to AWS"
+#     pass
+session = None
+bucket_manager = None
 
 @click.group()
-def cli():
-    "Webotron deploys websites to AWS"
-    pass
+@click.option('--profile', default=None,help="Use a given AWS profile.")
 
-@cli.command('list-buckets')#this is a decorator used for wrap function
+def cli(profile):
+    """Webotron deploys websites to AWS."""
+    global session, bucket_manager
+
+    session_cfg = {}
+    if profile:
+        session_cfg['profile_name'] = profile
+    session = boto3.Session(**session_cfg)
+    bucket_manager = BucketManager(session)
+
+@cli.command('list-buckets')
 def list_buckets():
-    "List all s3 buckets"
-    for bucket in s3.buckets.all():
+    """List all s3 buckets."""
+    for bucket in bucket_manager.all_buckets():
         print(bucket)
 
-@cli.command('list-bucket-objects')#this is a decorator used for wrap function
+@cli.command('list-bucket-objects')
 @click.argument('bucket')
 def list_bucket_objects(bucket):
-    "List objects in an s3 bucket"
-    for obj in s3.Bucket(bucket).objects.all():
+    """List objects in an s3 bucket."""
+    for obj in bucket_manager.all_objects(bucket):
         print(obj)
 
 @cli.command('setup-bucket')
 @click.argument('bucket')
 def setup_bucket(bucket):
-    "Create and configure s3 bucket"
-    s3_bucket = None
-    try:
-        s3_bucket = s3.create_bucket(
-        Bucket = bucket
-        )
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
-            s3_bucket = s3.Bucket(bucket)
-        else:
-            raise e
-
-    policy = """
-    {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-        "Sid": "PublicReadGetObject",
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": [ "s3:GetObject" ],
-        "Resource": [ "arn:aws:s3:::%s/*" ]
-            }
-        ]
-    }
-    """% s3_bucket.name
-
-    policy = policy.strip()
-    pol = s3_bucket.Policy()
-    pol.put(Policy=policy)
-
-    ws = s3_bucket.Website()
-    ws.put(WebsiteConfiguration={
-        'ErrorDocument': {
-                'Key': 'error.html'
-            },
-        'IndexDocument': {
-                'Suffix': 'index.html'
-            }
-        }
-        )
-
+    """Create and configure S3 bucket."""
+    s3_bucket = bucket_manager.init_bucket(bucket)
+    bucket_manager.set_policy(s3_bucket)
+    bucket_manager.configure_website(s3_bucket)
     return
-
-def upload_file(s3_bucket, path, key):
-    content_type = mimetypes.guess_type(key)[0] or 'text/plain'
-
-    s3_bucket.upload_file(
-        path,
-        key,
-        ExtraArgs={
-            'ContentType': 'text/html'
-        })
 
 @cli.command('sync')
 @click.argument('pathname', type=click.Path(exists=True))
 @click.argument('bucket')
 def sync(pathname, bucket):
-    "Sync contents of PATHNAME to BUCKET"
-    s3_bucket = s3.Bucket(bucket)
-
-    root = Path(pathname).expanduser().resolve()
-
-    def handle_directory(target):
-        for p in target.iterdir():
-            if p.is_dir(): handle_directory(p)
-            if p.is_file(): upload_file(s3_bucket, str(p), str(p.relative_to(root)))
-
-    handle_directory(root)
-
-    # root = Path(pathname).expanduser().resolve()
-    #
-    # def handle_directory(target):
-    #     for p in target.iterdir():
-    #         if p.is_dir(): handle_directory(p)
-    #         if p.is_file(): print("Path: {}\n Key: {}".format(p,p.relative_to(root)))
-    #
-    # handle_directory(root)
+    """Sync contents of PATHNAME to BUCKET."""
+    bucket_manager.sync(pathname, bucket)
 
 if __name__ == '__main__':
     cli()
