a979c29b2d7bf7cc8c376d3f26978546f02d95d3
Shawn Wong <luckywwx@gmail.com>
2020-06-09 13:24:39 +0800

    Set up Cloud Fronr CDN and SSL

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -5,6 +5,9 @@ from bucket import BucketManager
 
 from domain import DomainManager
 import util
+
+from certificate import CertificateManager
+from cdn import DistributionManager
 # session = boto3.Session(profile_name='pythonProject')
 # bucket_manager = BucketManager(session)
 # # s3 = session.resource('s3')
@@ -16,13 +19,15 @@ import util
 session = None
 bucket_manager = None
 domain_manager = None
+cert_manager = None
+dist_manager = None
 
 @click.group()
 @click.option('--profile', default=None,help="Use a given AWS profile.")
 
 def cli(profile):
     """Webotron deploys websites to AWS."""
-    global session, bucket_manager, domain_manager
+    global session, bucket_manager, domain_manager, cert_manager, dist_manager
 
     session_cfg = {}
     if profile:
@@ -30,6 +35,8 @@ def cli(profile):
     session = boto3.Session(**session_cfg)
     bucket_manager = BucketManager(session)
     domain_manager = DomainManager(session)
+    cert_manager = CertificateManager(session)
+    dist_manager = DistributionManager(session)
 
 @cli.command('list-buckets')
 def list_buckets():
@@ -65,7 +72,6 @@ def sync(pathname, bucket):
 @click.argument('domain')
 def setup_domain(domain):
     """Configure DOMAIN to point to BUCKET."""
-
     #Get a bucket by name.
     bucket = bucket_manager.get_bucket(domain)
 
@@ -73,7 +79,42 @@ def setup_domain(domain):
 
     endpoint = util.get_endpoint(bucket_manager.get_region_name(bucket))
     domain_manager.create_s3_domain_record(zone, domain, endpoint)
-    print("Domain configure: http://{}".format(domain))
+    print("Domain configured: http://{}".format(domain))
+
+@cli.command('find-cert')
+@click.argument('domain')
+def find_cert(domain):
+    """Find a certificate for <DOMAIN>."""
+    print(cert_manager.find_matching_cert(domain))
+
+
+@cli.command('setup-cdn')
+@click.argument('domain')
+@click.argument('bucket')
+def setup_cdn(domain, bucket):
+    """Set up CloudFront CDN for DOMAIN pointing to BUCKET."""
+    """根据domain name得到对应cloudFront的distribution"""
+    dist = dist_manager.find_matching_dist(domain)
+    #如果找不到这个distribution，就创一个。
+    if not dist:
+        cert = cert_manager.find_matching_cert(domain)
+        if not cert:  # SSL is not optional at this time
+            print("Error: No matching cert found.")
+            return
+
+        dist = dist_manager.create_dist(domain, cert)
+        #CloudFront的创建需要时间。
+        print("Waiting for distribution deployment...")
+        dist_manager.await_deploy(dist)
+
+    """找到domain name对应的zone"""
+    zone = domain_manager.find_hosted_zone(domain) or domain_manager.create_hosted_zone(domain)
+
+    """在domain name的host zone里创造一个cloudFront record"""
+    domain_manager.create_cf_domain_record(zone, domain, dist['DomainName'])
+    print("Domain configured: https://{}".format(domain))
+
+    return
 
 if __name__ == '__main__':
     cli()
