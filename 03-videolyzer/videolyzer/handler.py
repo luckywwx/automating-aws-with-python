e1918096c829126a19083814c9ede588a7fca51b
Shawn Wong <luckywwx@gmail.com>
2020-07-06 11:10:13 +0800

    Save data from get_label_detection to dynamoDB

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -50,8 +50,37 @@ def get_video_labels(job_id):
 
     return response
 
+def make_item(data):
+    if isinstance(data, dict):
+        return { k: make_item(v) for k, v in data.items() }
+        #if data is dict, make_item for each value in the dictionary
+
+    if isinstance(data, list):
+        return [ make_item(v) for v in data ]
+        #if data is list, make_item for each value in the list
+
+    if isinstance(data, float):
+        return str(data)
+        #if data is float, make it a string
+
+    return data
+
 def put_labels_in_db(data, video_name, video_bucket):
-    pass
+    del data['ResponseMetadata']
+    del data['JobStatus']
+
+    data['videoName'] = video_name
+    data['videoBucket'] = video_bucket
+
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ['DYNAMODB_TABLE_NAME']
+    videos_table = dynamodb.Table(table_name)
+
+    data = make_item(data)
+
+    videos_table.put_item(Item=data)
+
+    return
 
 def start_processing_video(event, context):
     for record in event['Records']:
